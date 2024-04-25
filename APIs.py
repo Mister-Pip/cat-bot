cbb3d1dc78ac58cf0cb339f09d33a6c977e2de02
Mister-Pip <dmitrius9797@gmail.com>
2024-04-25 17:20:12 +0300

    requiremets.txt added

---
+++ APIs.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+if not find_dotenv():
+    exit("Переменные окружения не загружены, так как отсутствует файл .env")
+else:
+    load_dotenv()
+
+BOT_TOKEN = os.getenv("TG_API")
+RAND_FACT_API = os.getenv("RAND_FACT_API")
